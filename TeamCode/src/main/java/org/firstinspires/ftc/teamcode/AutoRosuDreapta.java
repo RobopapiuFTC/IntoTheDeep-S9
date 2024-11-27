dca4613028ba936981e9db444a514bc1d99de0e2
Laptop programare 2 <laptop.robo@gmail.com>
2024-11-27 13:36:26 +0200

    init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRosuDreapta.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRosuDreapta.java
@@ -12,35 +12,37 @@ import org.firstinspires.ftc.teamcode.TankDrive;
 public final class AutoRosuDreapta extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Pose2d beginPose = new Pose2d(-12, 63, Math.toRadians(270));
+        Pose2d beginPose = new Pose2d(-12, 63, Math.toRadians(90));
             MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
 
             waitForStart();
 
             Actions.runBlocking(
                     drive.actionBuilder(beginPose)
-                            .strafeTo(new Vector2d(-47,40))
+                            .strafeTo(new Vector2d(-12,31))
                             .waitSeconds(0.5)
-                            .strafeToSplineHeading(new Vector2d(-50,50), Math.toRadians(135))
+                            .strafeToSplineHeading(new Vector2d(-37,40), Math.toRadians(180))
+                            .strafeToSplineHeading(new Vector2d(-37,5), Math.toRadians(180))
+                            .strafeToSplineHeading(new Vector2d(-45,5), Math.toRadians(180))
+                            .strafeToSplineHeading(new Vector2d(-45,55), Math.toRadians(180))
+                            .strafeToSplineHeading(new Vector2d(-45,5), Math.toRadians(180))
+                            .strafeToSplineHeading(new Vector2d(-54,5), Math.toRadians(180))
+                            .strafeToSplineHeading(new Vector2d(-54,55), Math.toRadians(180))
+                            .strafeToSplineHeading(new Vector2d(-37,55), Math.toRadians(270))
+                            .strafeToSplineHeading(new Vector2d(-12,31), Math.toRadians(90))
                             .waitSeconds(0.5)
-                            .strafeToSplineHeading(new Vector2d(-58,40), Math.toRadians(270))
+                            .strafeToSplineHeading(new Vector2d(-12,40), Math.toRadians(90))
+                            .strafeToSplineHeading(new Vector2d(-54,30), Math.toRadians(180))
+                            .strafeToSplineHeading(new Vector2d(-54,5), Math.toRadians(180))
+                            .strafeToSplineHeading(new Vector2d(-60,5), Math.toRadians(180))
+                            .strafeToSplineHeading(new Vector2d(-60,55), Math.toRadians(180))
+                            .strafeToSplineHeading(new Vector2d(-37,55), Math.toRadians(270))
+                            .strafeToSplineHeading(new Vector2d(-12,31), Math.toRadians(90))
                             .waitSeconds(0.5)
-                            .strafeToSplineHeading(new Vector2d(-50,50), Math.toRadians(135))
+                            .strafeToSplineHeading(new Vector2d(-37,55), Math.toRadians(270))
+                            .strafeToSplineHeading(new Vector2d(-12,31), Math.toRadians(90))
                             .waitSeconds(0.5)
-                            .strafeToSplineHeading(new Vector2d(-58,40), Math.toRadians(235))
-                            .waitSeconds(0.5)
-                            .strafeToSplineHeading(new Vector2d(-50,50), Math.toRadians(135))
-                            .waitSeconds(0.5)
-                            .strafeToSplineHeading(new Vector2d(-36,0), Math.toRadians(0))
-                            .strafeToSplineHeading(new Vector2d(-23,10), Math.toRadians(0))
-                            .waitSeconds(1)
-                            //.strafeToSplineHeading(new Vector2d(-30,0), Math.toRadians(0))
-                            .setTangent(Math.toRadians(90))
-                            .strafeToSplineHeading(new Vector2d(-45,8), Math.toRadians(0))
-                            .splineToLinearHeading(new Pose2d(0,50,Math.toRadians(0)), Math.toRadians(-10))
-                            .strafeToSplineHeading(new Vector2d(55,54), Math.toRadians(45))
-                            .waitSeconds(1)
-                            .strafeToSplineHeading(new Vector2d(-50,58), Math.toRadians(0))
+                            .strafeToSplineHeading(new Vector2d(-54,55), Math.toRadians(90))
                             .build());
 
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRosuStanga.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.TankDrive;
+@Autonomous(name = "AutoRosuStanga", group = "A")
+public final class AutoRosuStanga extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2d beginPose = new Pose2d(32, 63, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+        waitForStart();
+
+        Actions.runBlocking(
+                drive.actionBuilder(beginPose)
+                        .strafeTo(new Vector2d(12,32))
+                        .waitSeconds(0.5)
+                        .strafeToSplineHeading(new Vector2d(24,50), Math.toRadians(270))
+                        .strafeToSplineHeading(new Vector2d(48,38), Math.toRadians(270))
+                        .waitSeconds(0.5)
+                        .strafeToSplineHeading(new Vector2d(55,54), Math.toRadians(225))
+                        .waitSeconds(0.5)
+                        .strafeToSplineHeading(new Vector2d(57,38), Math.toRadians(270))
+                        .waitSeconds(0.5)
+                        .strafeToSplineHeading(new Vector2d(55,54), Math.toRadians(225))
+                        .waitSeconds(0.5)
+                        .strafeToSplineHeading(new Vector2d(58,37), Math.toRadians(315))
+                        .waitSeconds(0.5)
+                        .strafeToSplineHeading(new Vector2d(55,54), Math.toRadians(225))
+                        .waitSeconds(0.5)
+                        .strafeToSplineHeading(new Vector2d(55,10), Math.toRadians(180))
+                        .strafeToSplineHeading(new Vector2d(28,10), Math.toRadians(180))
+                        .waitSeconds(0.5)
+                        .strafeToSplineHeading(new Vector2d(55,10), Math.toRadians(180))
+                        .waitSeconds(0.5)
+                        .strafeToSplineHeading(new Vector2d(55,54), Math.toRadians(225))
+                        .build());
+
+    }
+}
